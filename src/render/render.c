ff0e53f461281eef7f0ab2c77866b5664a0d4f19
Pablo <pablodelolmodecasas@gmail.com>
2025-08-28 16:11:42 +0200

    fix ray direction while dividing render

--- src/render/render.c
+++ src/render/render.c
@@ -1,39 +1,48 @@
 #include "minirt.h"
 
+double	deg_to_rad(double x)
+{
+	return (x * M_PI / 180);
+}
+
+t_v3	get_ray_direction(t_scene *scene, int i, int j)
+{
+	double x = (2 * i - WIDTH + 1) * tan(deg_to_rad(scene->camera->fov * 0.5)) / WIDTH;
+	double y = (-2 * j + HEIGHT - 1) * tan(deg_to_rad(scene->camera->fov * 0.5)) / WIDTH;
+	t_v3 rd = v3_normalize(v3_add(v3_add(v3_scale(scene->camera->right, x), v3_scale(scene->camera->up, y)), scene->camera->orientation));
+	return (rd);
+}
+
+void	intersect_planes(t_scene *scene, t_v3 rd, double *closest, uint32_t *color)
+{
+	t_plane *plane = scene->plane_list;
+	while (plane)
+	{
+		double intersection = v3_dot_product(v3_substract(plane->coords, scene->camera->coords), plane->normal) / v3_dot_product(rd, plane->normal);
+		if (intersection > 0 && intersection < *closest)
+		{
+			*closest = intersection;
+			*color = plane->color.red * pow(256, 3) + plane->color.green * pow(256, 2) + plane->color.blue * pow(256, 1) + 255;
+		}
+		plane = plane->next;
+	}
+}
+
 void	render(t_scene *scene)
 {
-	double scale = tan(DEG_TO_RAD(scene->camera->fov / 2));
-	double step = 2.0 / WIDTH * scale;
-	double start_x = -scale + scale / WIDTH;
-	double start_y = scale / WIDTH * HEIGHT - scale / WIDTH;
-	double iter_y = start_y;
-
 	double i = 0;
 	while (i < WIDTH)
 	{
-		double iter_x = start_x;
 		double j = 0;
 		while (j < HEIGHT)
 		{
-			t_v3 rd = v3_normalize(v3_add(v3_add(v3_scale(scene->camera->right, iter_x), v3_scale(scene->camera->up, iter_y)), scene->camera->orientation));
-			float closest = 99999999999999999;
+			t_v3 rd = get_ray_direction(scene, i, j);
+			double closest = 99999999999999999;
 			uint32_t color;
-			t_plane *plane = scene->plane_list;
-			while (plane)
-			{
-				float intersection = v3_dot_product(v3_substract(plane->coords, scene->camera->coords), plane->normal) / v3_dot_product(rd, plane->normal);
-				if (intersection > 0 && intersection < closest)
-				{
-					closest = intersection;
-					color = plane->color.red * pow(256, 3) + plane->color.green * pow(256, 2) + plane->color.blue * pow(256, 1) + 255;
-				}
-				plane = plane->next;
-			}
-			iter_x += step;
-			j++;
+			intersect_planes(scene, rd, &closest, &color);
 			mlx_put_pixel(scene->img, i, j, color);
+			j++;
 		}
-		iter_y -= step;
 		i++;
 	}
 }
--- src/render/update_camera_axis.c
+++ src/render/update_camera_axis.c
@@ -1,10 +1,10 @@
 #include "minirt.h"
 
-//TODO special case: orientation is up
+//TODO special case: orientation is vertical
 void	update_camera_axis(t_camera *cam)
 {
 	cam->right = v3_normalize(v3_cross_product(cam->orientation, (t_v3){0, 1, 0}));
-	cam->up = v3_normalize(v3_cross_product(cam->orientation, cam->right));
+	cam->up = v3_normalize(v3_cross_product(cam->right, cam->orientation));
 	cam->forward = v3_normalize((t_v3){cam->orientation.x, 0, cam->orientation.z});
-	cam->rightward = v3_cross_product((t_v3){0, 1, 0}, cam->forward);
+	cam->rightward = v3_cross_product(cam->forward, (t_v3){0, 1, 0});
 }
