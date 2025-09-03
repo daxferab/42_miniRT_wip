710e6343aebba34c0a873d4a3eace382f8001185
daxferna <daxferna@student.42madrid.com>
2025-09-03 22:41:24 +0200

    Add: cylinder caps

--- src/render/render.c
+++ src/render/render.c
@@ -36,9 +36,39 @@ void	intersect_spheres(t_scene *scene, t_vector rd, double *closest, uint32_t *c
 		if (intersection > 0 && intersection < *closest)
 		{
 			*closest = intersection;
-			*color = sphere->color.red * pow(256, 3) + sphere->color.green * pow(256, 2) + sphere->color.blue * pow(256, 1) + 255;
+			*color = sphere->color.red * pow(256, 3) + sphere->color.green * pow(256, 2) + sphere->color.blue * pow(256, 1) + 128;
 		}
 		sphere = sphere->next;
+	}
+}
+void	intersect_cylinders(t_scene *scene, t_vector rd, double *closest, uint32_t *color)
+{
+	t_cylinder *cylinder = scene->cylinder_list;
+	while (cylinder)
+	{
+		t_coords	center_up = v3_add(cylinder->coords, v3_scale(cylinder->axis, cylinder->height / 2));
+		double	distance_up = v3_dot_product(v3_substract(center_up, scene->camera->coords), cylinder->axis) / v3_dot_product(rd, cylinder->axis);
+		t_coords intersection_up = v3_add(scene->camera->coords, v3_scale(rd, distance_up));
+		if (v3_magnitude(v3_substract(intersection_up, center_up)) <= cylinder->diameter / 2)
+		{
+			if (distance_up > 0 && distance_up < *closest)
+			{
+				*closest = distance_up;
+				*color = cylinder->color.red * pow(256, 3) + cylinder->color.green * pow(256, 2) + cylinder->color.blue * pow(256, 1) + 255;
+			}
+		}
+		t_coords	center_down = v3_substract(cylinder->coords, v3_scale(cylinder->axis, cylinder->height / 2));
+		double	distance_down = v3_dot_product(v3_substract(center_down, scene->camera->coords), cylinder->axis) / v3_dot_product(rd, cylinder->axis);
+		t_coords	intersection_down = v3_add(scene->camera->coords, v3_scale(rd, distance_up));;
+		if (v3_magnitude(v3_substract(intersection_down, center_down)) <= cylinder->diameter / 2)
+		{
+			if (distance_down > 0 && distance_down < *closest)
+			{
+				*closest = distance_down;
+				*color = cylinder->color.red * pow(256, 3) + cylinder->color.green * pow(256, 2) + cylinder->color.blue * pow(256, 1) + 255;
+			}
+		}
+		cylinder = cylinder->next;
 	}
 }
 
@@ -55,6 +85,7 @@ void	render(t_scene *scene)
 			uint32_t color = 255;
 			intersect_planes(scene, rd, &closest, &color);
 			intersect_spheres(scene, rd, &closest, &color);
+			intersect_cylinders(scene, rd, &closest, &color);
 			mlx_put_pixel(scene->img, i, j, color);
 			j++;
 		}
